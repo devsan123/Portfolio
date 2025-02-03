a248f4a9f9bfea2486aa43460d6f88be1442f33f
Devsan <devsandhadvi11@gmail.com>
2025-02-03 16:23:03 +0530

    update resume

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = () => {
               <li className="nav-item">
                 <a
                   className="nav-link"
-                  href="/assets/Resume/Devsan-Dhadavi-Resumee.pdf"
+                  href="/assets/Resume/Devsan-Dhadavi-Resume.pdf"
                   target="_blank"
                   // rel="noreferrer"
                   onClick={collapseNavbar}
